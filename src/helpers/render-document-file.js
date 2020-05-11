8b5a6185e6e211b0e07b9f1c1b7e23fb4b13dc9c
privateOmega <privateOmega@protonmail.com>
2020-05-11 20:10:51 +0530

    feat: added vdom to xml method

--- src/helpers/render-document-file.js
+++ src/helpers/render-document-file.js
@@ -1,24 +1,72 @@
 import { fragment } from 'xmlbuilder2';
 
-import { buildTextElement } from './xml-builder';
+import * as xmlBuilder from './xml-builder';
 
 const VNode = require('virtual-dom/vnode/vnode');
 const VText = require('virtual-dom/vnode/vtext');
+const isVNode = require('virtual-dom/vnode/is-vnode');
+const isVText = require('virtual-dom/vnode/is-vtext');
+const escape = require('escape-html');
 
 const convertHTML = require('html-to-vdom')({
   VNode,
   VText,
 });
 
-// eslint-disable-next-line no-unused-vars
-function convertVTreeToXML(vNode, xmlFragment) {
-  if (!vNode) {
+function findXMLEquivalent(docxDocumentInstance, vNode, xmlFragment) {
+  switch (vNode.tagName) {
+    case 'p':
+      xmlBuilder.buildParagraph(xmlFragment, vNode);
+      return;
+    case 'table':
+      xmlBuilder.buildTable(xmlFragment, vNode);
+      return;
+    case 'ol':
+    case 'ul':
+      // eslint-disable-next-line no-case-declarations
+      const numberingId = docxDocumentInstance.createOrderedList(vNode.tagName === 'ol');
+      // eslint-disable-next-line no-plusplus
+      for (let index = 0; index < vNode.children.length; index++) {
+        const childVNode = vNode.children[index];
+        if (childVNode.tagName === 'li') {
+          xmlBuilder.buildParagraph(xmlFragment, childVNode, {
+            numbering: { levelId: 0, numberingId },
+          });
+        }
+      }
+      return;
+
+    default:
+      break;
+  }
+  if (vNode.children && Array.isArray(vNode.children) && vNode.children.length) {
+    // eslint-disable-next-line no-plusplus
+    for (let index = 0; index < vNode.children.length; index++) {
+      const childVNode = vNode.children[index];
+      // eslint-disable-next-line no-use-before-define
+      convertVTreeToXML(docxDocumentInstance, childVNode, xmlFragment);
+    }
+  }
+}
+
+// eslint-disable-next-line consistent-return
+function convertVTreeToXML(docxDocumentInstance, vTree, xmlFragment) {
+  if (!vTree) {
     // eslint-disable-next-line no-useless-return
     return '';
   }
-  // eslint-disable-next-line no-empty
-  if (vNode.type === 'VirtualText') {
+  if (Array.isArray(vTree) && vTree.length) {
+    // eslint-disable-next-line no-plusplus
+    for (let index = 0; index < vTree.length; index++) {
+      const vNode = vTree[index];
+      convertVTreeToXML(docxDocumentInstance, vNode, xmlFragment);
+    }
+  } else if (isVNode(vTree)) {
+    findXMLEquivalent(docxDocumentInstance, vTree, xmlFragment);
+  } else if (isVText(vTree)) {
+    xmlBuilder.buildTextElement(xmlFragment, escape(String(vTree.text)));
   }
+  return xmlFragment;
 }
 
 function renderDocumentFile(docxDocumentInstance) {
