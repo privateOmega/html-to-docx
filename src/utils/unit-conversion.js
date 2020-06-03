5890b18833cc11f10c8ffc1e57d1dd9ffd46395d
privateOmega <privateOmega@protonmail.com>
2020-06-04 00:42:41 +0530

    fix: added unit conversions

--- src/utils/unit-conversion.js
+++ src/utils/unit-conversion.js
@@ -1,5 +1,6 @@
 export const pixelRegex = /([\d.]+)px/i;
 export const percentageRegex = /([\d.]+)%/i;
+export const pointRegex = /(\d+)pt/i;
 
 export const pixelsToEMU = (pixelValue) => {
   return Math.round(pixelValue * 9525);
@@ -15,4 +16,28 @@ export const TWIPToEMU = (TWIPValue) => {
 
 export const EMUToTWIP = (EMUValue) => {
   return Math.round(EMUValue / 635);
+};
+
+export const pointsToTWIP = (pointValue) => {
+  return Math.round(pointValue * 20);
+};
+
+export const TWIPToPoints = (TWIPValue) => {
+  return Math.round(TWIPValue / 20);
+};
+
+export const pointsToHIP = (pointValue) => {
+  return Math.round(pointValue * 2);
+};
+
+export const HIPToPoints = (HIPValue) => {
+  return Math.round(HIPValue / 2);
+};
+
+export const HIPToTWIP = (HIPValue) => {
+  return Math.round(HIPValue * 10);
+};
+
+export const TWIPToHIP = (TWIPValue) => {
+  return Math.round(TWIPValue / 10);
 };
