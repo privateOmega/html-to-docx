1ae584a1b0a5350943e10c0d129402b843d7b9a2
privateOmega <privateOmega@protonmail.com>
2020-06-08 10:13:58 +0530

    feat: added other measure units for margins and fonts

--- src/html-to-docx.js
+++ src/html-to-docx.js
@@ -3,6 +3,16 @@ import { create } from 'xmlbuilder2';
 import { relsXML } from './schemas';
 import DocxDocument from './docx-document';
 import { renderDocumentFile } from './helpers';
+import {
+  pixelRegex,
+  pixelToTWIP,
+  cmRegex,
+  cmToTWIP,
+  inchRegex,
+  inchToTWIP,
+  pointRegex,
+  pointToHIP,
+} from './utils/unit-conversion';
 
 const VNode = require('virtual-dom/vnode/vnode');
 const VText = require('virtual-dom/vnode/vtext');
@@ -40,11 +50,74 @@ const defaultDocumentOptions = {
 
 const mergeOptions = (options, patch) => ({ ...options, ...patch });
 
+const fixupFontSize = (fontSize) => {
+  let normalizedFontSize;
+  if (pointRegex.test(fontSize)) {
+    const matchedParts = fontSize.match(pointRegex);
+
+    normalizedFontSize = pointToHIP(matchedParts[1]);
+  } else if (fontSize) {
+    // assuming it is already in HIP
+    normalizedFontSize = fontSize;
+  } else {
+    normalizedFontSize = null;
+  }
+
+  return normalizedFontSize;
+};
+
+const fixupMargins = (margins) => {
+  let normalizedMargins = {};
+  if (typeof margins === 'object' && margins !== null) {
+    Object.keys(margins).forEach((key) => {
+      if (pixelRegex.test(margins[key])) {
+        const matchedParts = margins[key].match(pixelRegex);
+        normalizedMargins[key] = pixelToTWIP(matchedParts[1]);
+      } else if (cmRegex.test(margins[key])) {
+        const matchedParts = margins[key].match(cmRegex);
+        normalizedMargins[key] = cmToTWIP(matchedParts[1]);
+      } else if (inchRegex.test(margins[key])) {
+        const matchedParts = margins[key].match(inchRegex);
+        normalizedMargins[key] = inchToTWIP(matchedParts[1]);
+      } else if (margins[key]) {
+        normalizedMargins[key] = margins[key];
+      } else {
+        // incase value is something like 0
+        normalizedMargins[key] = defaultDocumentOptions.margins[key];
+      }
+    });
+  } else {
+    // eslint-disable-next-line no-param-reassign
+    normalizedMargins = null;
+  }
+
+  return normalizedMargins;
+};
+
+const normalizeDocumentOptions = (documentOptions) => {
+  const normalizedDocumentOptions = { ...documentOptions };
+  Object.keys(documentOptions).forEach((key) => {
+    // eslint-disable-next-line default-case
+    switch (key) {
+      case 'margins':
+        normalizedDocumentOptions.margins = fixupMargins(documentOptions[key]);
+        break;
+      case 'fontSize':
+      case 'complexScriptFontSize':
+        normalizedDocumentOptions[key] = fixupFontSize(documentOptions[key]);
+        break;
+    }
+  });
+
+  return normalizedDocumentOptions;
+};
+
 // Ref: https://en.wikipedia.org/wiki/Office_Open_XML_file_formats
 // http://officeopenxml.com/anatomyofOOXML.php
 // eslint-disable-next-line import/prefer-default-export
 export function addFilesToContainer(zip, htmlString, suppliedDocumentOptions, headerHTMLString) {
-  const documentOptions = mergeOptions(defaultDocumentOptions, suppliedDocumentOptions);
+  const normalizedDocumentOptions = normalizeDocumentOptions(suppliedDocumentOptions);
+  const documentOptions = mergeOptions(defaultDocumentOptions, normalizedDocumentOptions);
 
   const docxDocument = new DocxDocument({ zip, htmlString, ...documentOptions });
   // Conversion to Word XML happens here
