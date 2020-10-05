085ed2a2cb439ee2a4189b3664deca047926672b
privateOmega <privateOmega@protonmail.com>
2020-10-05 11:38:29 +0530

    feat: added page break support

--- src/helpers/render-document-file.js
+++ src/helpers/render-document-file.js
@@ -142,6 +142,26 @@ export const buildList = (vNode) => {
 };
 
 function findXMLEquivalent(docxDocumentInstance, vNode, xmlFragment) {
+  if (
+    vNode.tagName === 'div' &&
+    (vNode.properties.attributes.class === 'page-break' ||
+      (vNode.properties.style && vNode.properties.style['page-break-after']))
+  ) {
+    const paragraphFragment = fragment({
+      namespaceAlias: { w: namespaces.w },
+    })
+      .ele('@w', 'p')
+      .ele('@w', 'r')
+      .ele('@w', 'br')
+      .att('@w', 'type', 'page')
+      .up()
+      .up()
+      .up();
+
+    xmlFragment.import(paragraphFragment);
+    return;
+  }
+
   switch (vNode.tagName) {
     case 'h1':
     case 'h2':
