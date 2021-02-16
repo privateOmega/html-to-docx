ebe7ea54d3a055cc5e17dbb0b00fc942cc74f566
Robin <robin.bauhn@minso.se>
2021-02-16 11:17:02 +0100

    fix: formatted xml

--- src/schemas/styles.js
+++ src/schemas/styles.js
@@ -2,34 +2,33 @@ import { namespaces } from '../helpers';
 
 const generateStylesXML = (font = 'Times New Roman', fontSize = 22, complexScriptFontSize = 22) => {
   return `
-    <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+		<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 
-    <w:styles
-      xmlns:w="${namespaces.w}"
-      xmlns:r="${namespaces.r}" >
-        <w:docDefaults>
-          <w:rPrDefault>
-            <w:rPr>
-              <w:rFonts w:ascii="${font}" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi" w:cstheme="minorBidi" />
-              <w:sz w:val="${fontSize}"/>
-              <w:szCs w:val="${complexScriptFontSize}"/>
-              <w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/>
-            </w:rPr>
-          </w:rPrDefault>
-          <w:pPrDefault>
-            <w:pPr>
-              <w:spacing w:after="120" w:line="240" w:lineRule="atLeast"/>
-            </w:pPr>
-          </w:pPrDefault>
-        </w:docDefaults>
-        <w:style w:type="character" w:styleId="Hyperlink">
-          <w:name w:val="Hyperlink"/>
-          <w:rPr>
-            <w:color w:val="0000FF"/>
-            <w:u w:val="single"/>
-          </w:rPr>
-        </w:style>
-		  <w:style w:type="paragraph" w:styleId="Heading1">
+		<w:styles xmlns:w="${namespaces.w}"
+			xmlns:r="${namespaces.r}">
+			<w:docDefaults>
+				<w:rPrDefault>
+					<w:rPr>
+						<w:rFonts w:ascii="${font}" w:eastAsiaTheme="minorHAnsi" w:hAnsiTheme="minorHAnsi" w:cstheme="minorBidi" />
+						<w:sz w:val="${fontSize}"/>
+						<w:szCs w:val="${complexScriptFontSize}"/>
+						<w:lang w:val="en-US" w:eastAsia="en-US" w:bidi="ar-SA"/>
+					</w:rPr>
+				</w:rPrDefault>
+				<w:pPrDefault>
+					<w:pPr>
+						<w:spacing w:after="120" w:line="240" w:lineRule="atLeast"/>
+					</w:pPr>
+				</w:pPrDefault>
+			</w:docDefaults>
+			<w:style w:type="character" w:styleId="Hyperlink">
+				<w:name w:val="Hyperlink"/>
+				<w:rPr>
+					<w:color w:val="0000FF"/>
+					<w:u w:val="single"/>
+				</w:rPr>
+			</w:style>
+			<w:style w:type="paragraph" w:styleId="Heading1">
 				<w:name w:val="heading 1"/>
 				<w:basedOn w:val="Normal"/>
 				<w:next w:val="Normal"/>
@@ -144,7 +143,7 @@ const generateStylesXML = (font = 'Times New Roman', fontSize = 22, complexScrip
 					<w:szCs w:val="20"/>
 				</w:rPr>
 			</w:style>
-    </w:styles>
+		</w:styles>
   `;
 };
 
