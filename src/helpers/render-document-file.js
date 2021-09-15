a19ec884aed6f94eb745b5ac47ebdb0cc424bf71
privateOmega <privateOmega@protonmail.com>
2021-09-16 01:38:44 +0530

    feat: add support for code tag

--- src/helpers/render-document-file.js
+++ src/helpers/render-document-file.js
@@ -194,6 +194,7 @@ function findXMLEquivalent(docxDocumentInstance, vNode, xmlFragment) {
     case 'p':
     case 'a':
     case 'blockquote':
+    case 'code':
     case 'pre':
       const paragraphFragment = xmlBuilder.buildParagraph(vNode, {}, docxDocumentInstance);
       xmlFragment.import(paragraphFragment);
