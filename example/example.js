0a8d6e6a8cfeffd9543e17e7d4bc729dfde88ed4
privateOmega <privateOmega@protonmail.com>
2021-12-16 03:12:40 +0530

    feat(indentation): html margins to indentation #106

--- example/example.js
+++ example/example.js
@@ -54,6 +54,18 @@ const htmlString = `<!DOCTYPE html>
                 a type specimen book.
             </strong>
         </p>
+        <p style="margin-left: 40px;">
+            <strong>Left indented paragraph:</strong>
+            <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+        </p>
+        <p style="margin-right: 40px;">
+            <strong>Right indented paragraph:</strong>
+            <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+        </p>
+        <p style="margin-left: 40px; margin-right: 40px;">
+            <strong>Left and right indented paragraph:</strong>
+            <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+        </p>
         <ul style="list-style-type: circle;">
             <li>Unordered list element</li>
         </ul>
