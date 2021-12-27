a37adaebf46d06e88401512b6cc80d263611a943
Éric Renard <timmy469@gmail.com>
2021-12-27 17:59:57 +0100

    feat(font): register fonts in fontTable.xml

--- src/utils/font-family-conversion.js
+++ src/utils/font-family-conversion.js
@@ -1,12 +1,18 @@
 export const removeSimpleOrDoubleQuotes = /(["'])(.*?)\1/;
 
-export const fontFamilyToFontName = (fontFamilyString) => {
-  if (fontFamilyString) {
-    const firstFontName = fontFamilyString.split(',')[0];
-    if (removeSimpleOrDoubleQuotes.test(firstFontName)) {
-      return firstFontName.match(removeSimpleOrDoubleQuotes)[2];
-    }
-    return firstFontName;
-  }
-  return '';
+export const fontFamilyToTableObject = (fontFamilyString, fallbackFont) => {
+  const fontFamilyElements = fontFamilyString
+    ? fontFamilyString.split(',').map((fontName) => {
+        const trimmedFontName = fontName.trim();
+        if (removeSimpleOrDoubleQuotes.test(trimmedFontName)) {
+          return trimmedFontName.match(removeSimpleOrDoubleQuotes)[2];
+        }
+        return trimmedFontName;
+      })
+    : [fallbackFont];
+
+  return {
+    fontName: fontFamilyElements[0],
+    genericFontName: fontFamilyElements[fontFamilyElements.length - 1],
+  };
 };
