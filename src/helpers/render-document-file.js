2aa04ab8a66bf91fdd72de684481b2d9f640afbf
privateOmega <privateOmega@protonmail.com>
2021-12-28 23:21:39 +0530

    revert: reduce guard checks with optional chaining

--- src/helpers/render-document-file.js
+++ src/helpers/render-document-file.js
@@ -164,7 +164,7 @@ function findXMLEquivalent(docxDocumentInstance, vNode, xmlFragment) {
   if (
     vNode.tagName === 'div' &&
     (vNode.properties.attributes.class === 'page-break' ||
-      vNode.properties.style?.['page-break-after'])
+      (vNode.properties.style && vNode.properties.style['page-break-after']))
   ) {
     const paragraphFragment = fragment({ namespaceAlias: { w: namespaces.w } })
       .ele('@w', 'p')
