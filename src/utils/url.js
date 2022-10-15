e9b289b6f27f89ec9bc5219a57621987f9522fc5
privateOmega <privateOmega@protonmail.com>
2022-10-16 00:46:51 +0530

    fix: move url regex into the util function

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,8 @@
-const urlRegex = /http(s)?:\/\/(\w+:?\w*@)?(\S+)(:\d+)?((?<=\.)\w+)+(\/([\w#!:.?+=&%@!\-/])*)?/gi;
+const isValidUrl = (urlString) => {
+  const urlRegex = /http(s)?:\/\/(\w+:?\w*@)?(\S+)(:\d+)?((?<=\.)\w+)+(\/([\w#!:.?+=&%@!\-/])*)?/gi;
 
-const isValidUrl = (urlString) => Boolean(urlRegex.test(urlString));
+  return Boolean(urlRegex.test(urlString));
+};
 
-export { urlRegex, isValidUrl };
+// eslint-disable-next-line import/prefer-default-export
+export { isValidUrl };
